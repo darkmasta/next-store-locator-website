03f4e0d99480f4b42d502f447fdff1380e1a04ab
alperen <a.bagci@depaulisystems.com>
2024-02-04 22:48:28 +0300

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,15 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    breakpoints: {
+      'xxs': '290px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      "2xl": "1440px",
+      "3xl": "1920px",
+    },
   },
   plugins: [],
 };
